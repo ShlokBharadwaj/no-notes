c2917557f8208ff5a2af143c2af1b98227ec1166
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-11 17:06:09 +0530

    Add: StatefulWidget appBar under Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+    );
   }
 }
