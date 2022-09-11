024c989971a1a480501abd8458ab04b22d6e7d8e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-11 17:33:07 +0530

    Add: late variable email,password

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,16 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
 
   @override
   Widget build(BuildContext context) {
