4fce1f38cc7cb8da4dfb8832dee1a04bea9367c3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-11 18:52:07 +0530

    Add: Basic Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -47,45 +47,106 @@ class _HomePageState extends State<HomePage> {
         future: Firebase.initializeApp(
             options: DefaultFirebaseOptions.currentPlatform),
         builder: (context, snapshot) {
-          return Column(
-            children: [
-              TextField(
-                controller: _email,
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: "Email",
-                ),
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: "Password",
-                ),
-              ),
-              TextButton(
-                onPressed: () async {
-                  final email = _email.text;
-                  final password = _password.text;
-                  try {
-                    final UserCredential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                    print(UserCredential);
-                  } on FirebaseAuthException catch (e) {
-                    print(e);
-                  }
-                },
-                child: const Text("Register"),
-              ),
-            ],
-          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            // case ConnectionState.done:
+            //   return Column(
+            //     children: [
+            //       TextField(
+            //         controller: _email,
+            //         enableSuggestions: false,
+            //         autocorrect: false,
+            //         keyboardType: TextInputType.emailAddress,
+            //         decoration: const InputDecoration(
+            //           hintText: "Email",
+            //         ),
+            //       ),
+            //       TextField(
+            //         controller: _password,
+            //         obscureText: true,
+            //         enableSuggestions: false,
+            //         autocorrect: false,
+            //         decoration: const InputDecoration(
+            //           hintText: "Password",
+            //         ),
+            //       ),
+            //       TextButton(
+            //         onPressed: () async {
+            //           final email = _email.text;
+            //           final password = _password.text;
+            //           try {
+            //             final UserCredential = await FirebaseAuth.instance
+            //                 .createUserWithEmailAndPassword(
+            //               email: email,
+            //               password: password,
+            //             );
+            //             print(UserCredential);
+            //           } on FirebaseAuthException catch (e) {
+            //             print(e);
+            //           }
+            //         },
+            //         child: const Text("Register"),
+            //       ),
+            //     ],
+            //   );
+            default:
+              {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          controller: _email,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextField(
+                          controller: _password,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: _email.text,
+                                      password: _password.text);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('User registered'),
+                                ),
+                              );
+                            } on FirebaseAuthException catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(e.message ?? 'Error'),
+                                ),
+                              );
+                            }
+                          },
+                          child: const Text('Register'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              }
+          }
         },
       ),
     );
