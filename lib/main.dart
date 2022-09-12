7edf3670c1de6bab652d64b15b7aa3e031a1d30a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-13 01:29:13 +0530

    Add: Route for verify email

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ void main() {
       '/login': (context) => const LoginView(),
       '/register': (context) => const RegisterView(),
       '/notes': (context) => const NotesView(),
+      '/verify-email': (context) => const VerifyEmailView(),
     },
     // darkTheme: ThemeData.dark(),
   ));
