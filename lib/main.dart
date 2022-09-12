30483e21bbe3e8323eaa0b07788aa2e1c3bc7e74
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-12 18:45:34 +0530

    Add: Routes for login and regiser page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nonotes/views/login_view.dart';
+import 'package:nonotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -13,6 +14,10 @@ void main() {
       primarySwatch: Colors.deepPurple,
     ),
     home: const HomePage(),
+    routes: {
+      '/login': (context) => const LoginView(),
+      '/register': (context) => const RegisterView(),
+    },
     // darkTheme: ThemeData.dark(),
   ));
 }
