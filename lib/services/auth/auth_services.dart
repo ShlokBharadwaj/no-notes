563f9b727e86aa07f75ed7650c7a9df11b04500b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-13 21:02:32 +0530

    Add: factory to use own Auth provider

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -1,11 +1,16 @@
 import 'package:nonotes/services/auth/auth_provider.dart';
 import 'package:nonotes/services/auth/auth_user.dart';
+import 'package:nonotes/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   @override
   final AuthProvider provider;
 
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(
+        FirebaseAuthProvider(),
+      );
 
   @override
   Future<AuthUser> createUser({
