b83c67124909887189cc00a909cbecbf29f3430d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-14 12:49:23 +0530

    Add: Mocked user and current user status

---
+++ test/auth_test.dart
@@ -0,0 +1,68 @@
+import 'package:nonotes/services/auth/auth_provider.dart';
+import 'package:nonotes/services/auth/auth_user.dart';
+import 'package:test/test.dart';
+
+void main() {}
+
+class NotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+  AuthUser? _user;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!isInitialized) throw NotInitializedException();
+    await Future.delayed(const Duration(milliseconds: 1000));
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  AuthUser? get currentUser => _user;
+
+  @override
+  Future<AuthUser> deleteUser() {
+    // TODO: implement deleteUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> initialize() {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendPasswordResetEmail({required String email}) {
+    // TODO: implement sendPasswordResetEmail
+    throw UnimplementedError();
+  }
+}
