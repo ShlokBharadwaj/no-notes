7198b2f48ffed95f0bdd1a2884b4d8beaaf9082c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-15 18:09:45 +0530

    Add: Delete all notes from cache and update UI

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -66,7 +66,10 @@ class NotesService {
 
   Future<int> deleteAllNotes() async {
     final db = _getDatabaseOrThrow();
-    return await db.delete(noteTable);
+    final numberOfDeletions = await db.delete(noteTable);
+    _notes = [];
+    _notesStreamController.add(_notes);
+    return numberOfDeletions;
   }
 
   Future<void> deleteNote({required int id}) async {
