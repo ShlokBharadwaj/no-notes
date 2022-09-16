65b135afaa7fb4b5b7ea3f2e6afe730253a6f653
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-16 17:16:20 +0530

    Add: Delete notes if text is empty

--- lib/views/notes/new_notes_view.dart
+++ lib/views/notes/new_notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nonotes/services/auth/auth_services.dart';
+import 'package:nonotes/services/crud/notes_services.dart';
 
 class NewNotesView extends StatefulWidget {
   const NewNotesView({super.key});
@@ -8,6 +10,33 @@ class NewNotesView extends StatefulWidget {
 }
 
 class _NewNotesViewState extends State<NewNotesView> {
+  DatabaseNote? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textEditingController;
+
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final owner = await _notesService.getUser(
+      email: currentUser.email!,
+    );
+    return await _notesService.createNote(
+      owner: owner,
+    );
+  }
+
+  void _deleteNoteIfTextIsEmpty() async {
+    final note = _note;
+    if (note != null && _textEditingController.text.isEmpty) {
+      await _notesService.deleteNote(
+        id: note.id,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
