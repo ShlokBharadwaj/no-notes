349bbb048157a1904753abcd19803573e12123fd
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-16 12:45:24 +0530

    Add: Email for storing in local database

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,17 +4,17 @@ import 'package:flutter/foundation.dart';
 @immutable
 class AuthUser {
   // final String id;
-  // final String email;
+  final String? email;
   final bool isEmailVerified;
   const AuthUser({
     // required this.id,
-    // required this.email,
+    required this.email,
     required this.isEmailVerified,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
         // id: user.uid,
-        // email: user.email!,
+        email: user.email!,
         isEmailVerified: user.emailVerified,
       );
 }
