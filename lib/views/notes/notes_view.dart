9277dd29e5be23933b0a3c3e41bbca03d9e77718
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-17 16:16:25 +0530

    Add: Delete notes

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -3,6 +3,9 @@ import 'package:nonotes/constants/routes.dart';
 import 'package:nonotes/enums/menu_action.dart';
 import 'package:nonotes/services/auth/auth_services.dart';
 import 'package:nonotes/services/crud/notes_services.dart';
+import 'package:nonotes/utilities/dialogs/delete_user_dialog.dart';
+import 'package:nonotes/utilities/dialogs/logout_dialog.dart';
+import 'package:nonotes/views/notes/notes_list_view.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -76,17 +79,11 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final allNotes = snapshot.data as List<DatabaseNote>;
-                        return ListView.builder(
-                          itemCount: allNotes.length,
-                          itemBuilder: (context, index) {
-                            final note = allNotes[index];
-                            return ListTile(
-                              title: Text(
-                                note.text,
-                                maxLines: 1,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
+                        return NotesListView(
+                          notes: allNotes,
+                          onDeleteNote: (note) async {
+                            await _notesService.deleteNote(
+                              id: note.id,
                             );
                           },
                         );
@@ -117,44 +114,44 @@ class _NotesViewState extends State<NotesView> {
   }
 }
 
-Future<bool> showLogoutDialog(BuildContext context) async {
-  return showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Logout"),
-          content: const Text("Are you sure you want to logout?"),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text("Logout"),
-            ),
-          ],
-        );
-      }).then((value) => value ?? false);
-}
+// Future<bool> showLogoutDialog(BuildContext context) async {
+//   return showDialog<bool>(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: const Text("Logout"),
+//           content: const Text("Are you sure you want to logout?"),
+//           actions: [
+//             TextButton(
+//               onPressed: () => Navigator.of(context).pop(false),
+//               child: const Text("Cancel"),
+//             ),
+//             TextButton(
+//               onPressed: () => Navigator.of(context).pop(true),
+//               child: const Text("Logout"),
+//             ),
+//           ],
+//         );
+//       }).then((value) => value ?? false);
+// }
 
-Future<bool> showDeleteUserDialog(BuildContext context) async {
-  return showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Delete User"),
-          content: const Text("Are you sure you want to delete your user?"),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text("Delete User"),
-            ),
-          ],
-        );
-      }).then((value) => value ?? false);
-}
+// Future<bool> showDeleteUserDialog(BuildContext context) async {
+//   return showDialog<bool>(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: const Text("Delete User"),
+//           content: const Text("Are you sure you want to delete your user?"),
+//           actions: [
+//             TextButton(
+//               onPressed: () => Navigator.of(context).pop(false),
+//               child: const Text("Cancel"),
+//             ),
+//             TextButton(
+//               onPressed: () => Navigator.of(context).pop(true),
+//               child: const Text("Delete User"),
+//             ),
+//           ],
+//         );
+//       }).then((value) => value ?? false);
+// }
