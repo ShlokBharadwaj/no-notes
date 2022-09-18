dd436b88c919a19de93b8609ba349089833a71a4
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-18 20:24:41 +0530

    Add: Fetch all notes from Firestore

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection('notes');
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+  factory FirebaseCloudStorage() => _shared;
+}
