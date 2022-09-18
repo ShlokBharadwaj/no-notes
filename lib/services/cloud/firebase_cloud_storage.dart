7a6321e3f75f02a0e866b06733ce6e7931a076dc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-18 20:28:05 +0530

    Add: Create new notes in Firestore database

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,7 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nonotes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
+
+  void createNewNote({
+    required String ownerUserId,
+    // required String text,
+  }) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+
   static final FirebaseCloudStorage _shared =
       FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
