29436546e468ca0d262e18531df30d0b2a835eef
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-18 20:01:53 +0530

    Add: Firestore storage exceptions

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,19 @@
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+class CouldNotCreateNoteException extends CloudStorageException {
+  const CouldNotCreateNoteException();
+}
+
+class CouldNotGetAllNotesException extends CloudStorageException {
+  const CouldNotGetAllNotesException();
+}
+
+class CouldNotUpdateNoteException extends CloudStorageException {
+  const CouldNotUpdateNoteException();
+}
+
+class CouldNotDeleteNoteException extends CloudStorageException {
+  const CouldNotDeleteNoteException();
+}
