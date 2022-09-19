e24d4f49852df38beee01b5e5b2988f3242c66dc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-19 18:46:37 +0530

    Update: Notes list view now use Iterable instead of List

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:nonotes/services/crud/notes_services.dart';
+import 'package:nonotes/services/cloud/cloud_note.dart';
 import 'package:nonotes/utilities/dialogs/delete_note_dialog.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
 
@@ -21,7 +21,7 @@ class NotesListView extends StatelessWidget {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        final note = notes[index];
+        final note = notes.elementAt(index);
         return ListTile(
           onTap: () {
             onTap(note);
