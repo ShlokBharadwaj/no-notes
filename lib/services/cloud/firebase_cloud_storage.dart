2da680657092ad8eecb9fe219302d51566052cd6
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-19 16:42:12 +0530

    Add: Get notes from firestore by user Id

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,8 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nonotes/services/cloud/cloud_note.dart';
 import 'package:nonotes/services/cloud/cloud_storage_constants.dart';
+import 'package:nonotes/services/cloud/cloud_storage_exceptions.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then(
+            (value) => value.docs.map(
+              (doc) {
+                return CloudNote(
+                  documentId: doc.id,
+                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                  text: doc.data()[textFieldName] as String,
+                );
+              },
+            ),
+          );
+    } catch (e) {
+      throw const CouldNotGetAllNotesException();
+    }
+  }
 
   void createNewNote({
     required String ownerUserId,
