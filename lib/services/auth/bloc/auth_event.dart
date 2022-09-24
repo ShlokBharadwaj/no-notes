51715df377ff06dc9a529035dd03a59037a76626
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-24 11:22:06 +0530

    Add: Auth event for Initialization, in bloc

---
+++ lib/services/auth/bloc/auth_event.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class AuthEvent {
+  const AuthEvent();
+}
+
+class AuthEventInitialize extends AuthEvent {
+  const AuthEventInitialize();
+}
