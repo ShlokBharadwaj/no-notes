766dd19cc2c2ac8b80115ebf3a58a2413ae1389a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-26 18:45:37 +0530

    Update: Auth event to Initialize the app

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -32,17 +32,24 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     );
 
     // Initialization
-    on<AuthEventInitialize>(((event, emit) async {
-      await provider.initialize();
-      final user = provider.currentUser;
-      if (user == null) {
-        emit(const AuthStateLoggedOut(null));
-      } else if (!user.isEmailVerified) {
-        emit(const AuthStateNeedsEmailVerification());
-      } else {
-        emit(AuthStateLoggedIn(user));
-      }
-    }));
+    on<AuthEventInitialize>(
+      ((event, emit) async {
+        await provider.initialize();
+        final user = provider.currentUser;
+        if (user == null) {
+          emit(
+            const AuthStateLoggedOut(
+              exception: null,
+              isLoading: false,
+            ),
+          );
+        } else if (!user.isEmailVerified) {
+          emit(const AuthStateNeedsEmailVerification());
+        } else {
+          emit(AuthStateLoggedIn(user: user));
+        }
+      },),
+    );
     on<AuthEventLogIn>((event, emit) async {
       // emit(const AuthStateLoading());
       final email = event.email;
