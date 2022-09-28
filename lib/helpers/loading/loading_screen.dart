1f29e269ecce7e1bb51a668b91cc30c8f1061ccc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-28 19:47:29 +0530

    Update: Loading screen textbox dimentions

--- lib/helpers/loading/loading_screen.dart
+++ lib/helpers/loading/loading_screen.dart
@@ -43,40 +43,43 @@ class LoadingScreen {
       context,
     ) {
       return Material(
-        color: Colors.transparent,
+        color: Colors.black.withAlpha(150),
         child: Center(
           child: Container(
             constraints: BoxConstraints(
               maxWidth: size.width * 0.8,
               maxHeight: size.height * 0.8,
               minWidth: size.width * 0.5,
-              minHeight: size.height * 0.5,
             ),
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(10),
             ),
-            color: Colors.black.withOpacity(0.5),
-            child: Center(
-              child: StreamBuilder<String>(
-                stream: _text.stream,
-                builder: (context, snapshot) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const CircularProgressIndicator(),
-                      const SizedBox(
-                        width: 20,
-                        height: 20,
-                      ),
-                      Text(
-                        snapshot.data as String,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  );
-                },
-              ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  const CircularProgressIndicator(),
+                  const SizedBox(height: 20),
+                  StreamBuilder(
+                    stream: _text.stream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text(
+                          snapshot.data as String,
+                          textAlign: TextAlign.center,
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                ],
+              )),
             ),
           ),
         ),
