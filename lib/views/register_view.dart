4a0f86e69c71183ba08cc27d39753e7e388b5cb7
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-30 07:47:12 +0530

    Remove: Unused imports in Register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nonotes/constants/routes.dart';
 import 'package:nonotes/services/auth/auth_exception.dart';
-import 'package:nonotes/services/auth/auth_services.dart';
 import 'package:nonotes/services/auth/bloc/auth_bloc.dart';
 import 'package:nonotes/services/auth/bloc/auth_event.dart';
 import 'package:nonotes/services/auth/bloc/auth_state.dart';
