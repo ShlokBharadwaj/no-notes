d272f11e1046d4f669c42e47c21962e8be380fba
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-30 07:48:53 +0530

    Add: Autofocus in Register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -58,6 +58,7 @@ class _RegisterViewState extends State<RegisterView> {
                 controller: _email,
                 enableSuggestions: false,
                 autocorrect: false,
+                autofocus: true,
                 keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
